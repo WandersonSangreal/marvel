ae4075073c40cfd46d66e7e61a809b349d44641a
Wanderson Sangreal <sangreal.wanderson@hotmail.com>
2022-01-08 15:52:34 -0300

    Adaptações para chamadas na API da Marvel

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,8 @@
 export const environment = {
-  production: true
+  production: true,
+  crendential: {
+    public: '5a237863b3cc2061003cbbc4fe20dc06',
+    ts: 'fbf255068eccea6d0ef951b9f25626b57ab2fe72',
+    hash: 'e5116ac2dbdaa8d6fb1642631fde2588',
+  }
 };
