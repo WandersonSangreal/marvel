2efd968a02763939fdc4e9495fb1db51205871d7
Wanderson Sangreal <sangreal.wanderson@hotmail.com>
2022-01-11 19:20:08 -0300

    Thumbnails dos quadrinhos

--- src/app/main/detailed/detailed.component.ts
+++ src/app/main/detailed/detailed.component.ts
@@ -2,6 +2,11 @@ import {Component, OnInit} from '@angular/core';
 import {Observable, of} from "rxjs";
 import {ApiService} from "../../services/api.service";
 import {ActivatedRoute} from "@angular/router";
+import {Character} from "../../models/character";
+import {Comic} from "../../models/comic";
+import {Pagination} from "../../models/pagination";
+import {environment} from "../../../environments/environment";
+import {map} from "rxjs/operators";
 
 @Component({
   selector: 'app-detailed',
@@ -10,20 +15,28 @@ import {ActivatedRoute} from "@angular/router";
 })
 export class DetailedComponent implements OnInit {
 
-  public details$: Observable<any> = of(null);
+  public details$: Observable<{ results: Array<Character>; pagination: Pagination } | null> = of(null);
+  public comics$: Observable<{ results: Array<Comic>; pagination: Pagination } | null> = of(null);
 
   public constructor(private route: ActivatedRoute, private apiService: ApiService) {
   }
 
   public ngOnInit(): void {
 
-    this.details$ = this.apiService.get<any>('characters/' + this.route.snapshot.params['id']);
+    this.details$ = this.apiService.get<Array<Character>>('characters/' + this.route.snapshot.params['id']).pipe(map(response => {
+      if (response.results.length > 0 && response.results[0]?.comics?.collectionURI) {
+        this.getComicThumb(response.results[0]?.comics?.collectionURI);
+      }
+      return response;
+    }));
 
   }
 
-  public getComicThumb(url: string): Observable<any> {
+  public getComicThumb(url: string): void {
 
-    return this.apiService.get<any>(url);
+    const endpoint = url.replace('http', 'https').replace(environment.api, '');
+
+    this.comics$ = this.apiService.get<Array<Comic>>(endpoint);
 
   }
 
